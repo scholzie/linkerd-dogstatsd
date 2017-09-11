d8942d07cfc866c28d4e56c22599608bc5e1f527
Chris Scholz <chris.scholz@blueapron.com>
2017-09-11 08:18:45 -0400

    Use pattern matching for mkName

--- src/main/scala/com/blueapron/linkerd/telemetry/dogstatsd/DogstatsDStatsReceiver.scala
+++ src/main/scala/com/blueapron/linkerd/telemetry/dogstatsd/DogstatsDStatsReceiver.scala
@@ -9,11 +9,39 @@ import scala.collection.JavaConverters._
 private[telemetry] object DogstatsDStatsReceiver {
   // TODO: Work through metric name scenarios for comprehensive tagging and
   //       statistic naming
-  private[dogstatsd] def mkName(name: Seq[String]): (String, Seq[String]) = {
+  private[dogstatsd] def mkDefaultName(name: Seq[String]): (String, Seq[String]) = {
     val tags = new ListBuffer[String]()
     val nameBuf = StringBuilder.newBuilder
-    nameBuf.append("linkerd")
 
+    tags += s"app:unknown"
+
+    (nameBuf.toString, tags.toSeq)
+  }
+
+  private[dogstatsd] def mkNamerName(name: Seq[String]): (String, Seq[String]) = {}
+
+  private[dogstatsd] def mkRouterName(name: Seq[String]): (String, Seq[String]) = {
+    val nameBuf = StringBuilder.newBuilder
+    val tags = new ListBuilder[String]()
+    nameBuf.append("linkerd.routers")
+
+    val routerLabel = name(3)
+    tags += s"souter:$routerLabel"
+
+    if name(5) == "server" {
+      nameBuf.append(".servers")
+      val listenerAddress = name(6)
+      val listenerPort = name(7)
+      tags += s"linkerd_listener:$serverAddress:$serverPort"
+    } else {
+      nameBuf.append(".interpreter")
+      val interpreter = cleanName(5)
+      tags += s"linkerd_interpreter:$interpreter"
+    }
+    nameBuf.append(name.last) // append the stat itself
+  }
+
+  private[dogstatsd] def mkName(name: Seq[String]): (String, Seq[String]) = {
     var cleanName = name.mkString("/")
       .replaceAll("[^/A-Za-z0-9]", "_")
       .replaceAll("/{2,}", "/")
@@ -21,29 +49,14 @@ private[telemetry] object DogstatsDStatsReceiver {
       .replaceAll("\\.{2,}", ".")
       .split(".")
 
-    if (cleanName(1) == "rt") {
-      nameBuf.append(".routers")
-      val routerLabel = cleanName(2)
-      tags += s"router:$routerLabel"
-
-      if (cleanName(4) == "server") {
-        nameBuf.append(".servers")
-        val serverAddress = cleanName(5)
-        val serverPort = cleanName(6)
-        tags += s"linkerd_listener:$serverAddress:$serverPort"
-      } else {
-        nameBuf.append(".interpreter")
-        val interpreter = cleanName(4)
-        tags += s"linkerd_interpreter:$interpreter"
-      }
-
-      nameBuf.append(cleanName.last) // the statistic itself
-    } else {
-      // Catch-all for other options not picked up by the above
-      nameBuf.append(cleanName.mkString("."))
+    cleanName(2) match {
+        case "namerd" => mkDefaultName(cleanName) // need to properly parse router names first
+        case "rt"     => mkDefaultName(cleanName) // and namer names as well.
+        case _        => mkDefaultName(cleanName)
     }
+    val metricName, tags = parseName(cleanName)
 
-    (nameBuf.toString, tags.toSeq)
+    (metricName, tags)
   }
 }
 
